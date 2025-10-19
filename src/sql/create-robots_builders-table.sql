35f19fae3dad352330ec8940a6d14c18a66f8d11
clee-13 <caedylee@gmail.com>
2025-10-19 11:59:18 -0400

    edited tables, created supabase migrations

--- src/sql/create-robots_builders-table.sql
+++
@@ -1,6 +0,0 @@
--- join table
-CREATE TYPE robots_builders (
-    robot_id REFERENCES robots (robot_id),
-    builder_id REFERENCES builders (builder_id),
-    PRIMARY KEY (robot_id, builder_id)
-);
